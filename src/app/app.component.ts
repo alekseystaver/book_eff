530ef7bece02157d6c5792f17f5c7791093fc10b
alekseystaver <leksysstaver@gmail.com>
2025-11-14 11:50:17 +0300

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'books-efficiently';
+
+  constructor(private router: Router){}
+
+    public goToBookList() {
+    this.router.navigate(['/']);
+  }
 }
